e0b4592be54c478992b108522d9eb8895e4527d0
Brodypen <brodypen7987@gmail.com>
2023-07-13 14:25:01 -0500

    feat: New Auth form

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
-// import { UserAuthForm } from "@/app/examples/authentication/components/user-auth-form"
+import AuthForm from "@/components/AuthForm"
 import blub from "@/assets/absurd_design_bulb.png"
 import { Lightbulb } from "lucide-react"
 
@@ -17,14 +17,7 @@ export default function AuthenticationPage() {
   return (
     <>
       <div className="container relative h-screen max-h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-3 lg:px-0">
-        {/* <Link
-          href="/signin"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}        >
-          Login
-        </Link> */}
+
         <div className="col-span-1 relative hidden h-full flex-col bg-muted p-10 text-black dark:border-r lg:flex">
           <div className="absolute inset-0 bg-gradient-to-r from-rose-100 to-teal-100" />
           <div className="relative z-20 flex items-center text-lg font-medium">
@@ -51,16 +44,7 @@ export default function AuthenticationPage() {
         </div>
         <div className="lg:p-8 col-span-2">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] ">
-            <div className="flex flex-col space-y-2 text-center ">
-              <h1 className="text-3xl font-semibold tracking-tight">
-                Join Better Pomodoro!
-              </h1>
-
-              <p className="text-sm text-muted-foreground">
-                A better way to Pomodoro
-              </p>
-            </div>
-            {/* <UserAuthForm /> */}
+            <AuthForm mode="register"/>
             <p className="px-8 text-center text-sm text-muted-foreground">
               Already have an account?{" "}
               <Link
