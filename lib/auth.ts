c4299663eae0bcf799418cca573ccef6b3294c09
Brodypen <brodypen7987@gmail.com>
2023-07-15 08:11:45 -0500

    refactor: auth types

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import { SignJWT, jwtVerify } from "jose";
 import { db } from "./db";
 
-export const hashPassword = (password) => bcrypt.hash(password, 10);
+export const hashPassword = (password: string | Buffer) => bcrypt.hash(password, 10);
 
-export const comparePasswords = (plainTextPassword, hashedPassword) =>
+export const comparePasswords = (plainTextPassword: string | Buffer, hashedPassword: string) =>
   bcrypt.compare(plainTextPassword, hashedPassword);
